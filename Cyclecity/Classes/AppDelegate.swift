1c53cd2f55a6b6ce162286beff375feb1c416e83
Andrew Hao <andrewhao@gmail.com>
2015-05-26 16:01:43 -0700

    Small indentation fixes.

--- Cyclecity/Classes/AppDelegate.swift
+++ Cyclecity/Classes/AppDelegate.swift
@@ -1,22 +1,19 @@
-//
 //  AppDelegate.swift
 //  Cyclecity
 //
 //  Created by Andrew Hao on 3/4/15.
 //  Copyright (c) 2015 g9Labs. All rights reserved.
-//
 
 import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    var window: UIWindow?
+  var window: UIWindow?
 
-    let appDependencies = AppDependencies()
+  let appDependencies = AppDependencies()
 
-    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-      appDependencies.installRootViewControllerIntoWindow(window!)
-
-      return true
-    }
+  func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+    appDependencies.installRootViewControllerIntoWindow(window!)
+    return true
+  }
 }
