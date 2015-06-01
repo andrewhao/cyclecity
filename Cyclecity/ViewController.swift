792f36dbf7879af54c826c823a204e9d0e74b080
Andrew Hao <andrewhao@gmail.com>
2015-06-01 11:04:56 -0700

    Reincarnation v2

---
+++ Cyclecity/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Cyclecity
+//
+//  Created by Andrew Hao on 6/1/15.
+//  Copyright (c) 2015 g9Labs Design Studio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
